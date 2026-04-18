e6574936f8735caeee5e4f849f8b0500d0515480
ramvq <ramvqcraft@gmail.com>
2026-04-17 23:58:35 -0600

    added propeller spin

--- App/Webclient/WebSandbox/test/drone2.gd
+++ App/Webclient/WebSandbox/test/drone2.gd
@@ -5,18 +5,30 @@ const C = preload("res://constants.gd")
 var pollux : Node3D
 var left_aka : Node3D
 var right_aka : Node3D
+var main_propeller : Node3D
+var side_propeller_1 : Node3D
+var side_propeller_2 : Node3D
 
 var velocity : Vector3 = Vector3.ZERO 	#linear translation
 var angular_velocity : float = 0.0		#rotational movement
 
 func _ready():
+	
+	# loading drone hierarchy
 	pollux = $Pollux
-	left_aka = $Pollux/Pollux_ver8/Hull/LeftAka
-	right_aka = $Pollux/Pollux_ver8/Hull/RightAka
+	left_aka = $Pollux/Hull/LeftAka
+	right_aka = $Pollux/Hull/RightAka
+
+	main_propeller = $"Pollux/Hull/MidBody/Thruster/U92(ApisQueen)/Propeller001"
+	side_propeller_1 = $"Pollux/Hull/MidBody/Thruster/U2(ApisQueen)_Fore/Propeller001001"
+	side_propeller_2 = $"Pollux/Hull/MidBody/Thruster/U2(ApisQueen)_After/Propeller0010012"
+
+
 
 func _process(delta):
 	handle_translation(delta)
 	handle_rotation(delta)
+	handle_propellers(delta)
 
 func handle_translation(delta):
 	var input_dir = Vector3.ZERO
@@ -51,3 +63,13 @@ func handle_rotation(delta):
 
 	# Apply rotation
 	pollux.rotate_y(deg_to_rad(angular_velocity) * delta)
+	
+func handle_propellers(delta):
+	# Spin main propeller based on forward/backward velocity
+	var main_spin = velocity.length() * C.MAIN_PROPELLER_SPIN
+	main_propeller.rotate_z(deg_to_rad(main_spin) * delta)
+
+	# Spin side propellers based on angular velocity
+	var side_spin = abs(angular_velocity) * C.SIDE_PROPELLER_SPIN
+	side_propeller_1.rotate_z(deg_to_rad(side_spin) * delta)
+	side_propeller_2.rotate_z(deg_to_rad(side_spin) * delta)	
